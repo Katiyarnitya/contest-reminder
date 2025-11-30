aa55a129a2b3f8aaef91eebe5e95fbf520d7a1b2
Katiyarnitya <137599361+Katiyarnitya@users.noreply.github.com>
2025-12-01 00:37:01 +0530

    Modified contestUpdater.js file

--- backend/jobs/contestUpdater.js
+++ backend/jobs/contestUpdater.js
@@ -1,2 +1,33 @@
 import axios from "axios";
 import { Contest } from "../models/Contest";
+
+// leetcode contest fetching
+const fetchLeetCodeContests = async () => {
+  try {
+    const response = await axios.post("https://leetcode.com/graphql", {
+      query: `
+          query {
+            allContests {
+              title
+              titleSlug
+              startTime
+              duration
+            }
+          }
+        `,
+    });
+    return response.data.data.allContests.map((c) => ({
+      //  Array of contest objects
+      name: c.title,
+      slung: c.titleSlung,
+      startTime: new Date(c.startTime * 1000),
+      endTime: new Date((c.startTime + c.duration) * 1000),
+      url: `https://leetcode.com/contest/${c.titleSlug}`,
+    }));
+  } catch (error) {
+    console.error("Error fetching LeetCode contests:", err.message);
+    return [];
+  }
+};
+
+// For codeforces
